4d7b39c07390f89319313a59e26d6bb4f05bb6b6
Heyscar Romero <heyscarromero@gmail.com>
2023-11-26 23:44:46 -0300

    debug vscode

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }] },}
+const nextConfig = { images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }, { protocol: "https", hostname: "raw.githubusercontent.com", pathname:"/PokeAPI/sprites/master/sprites/pokemon/**" },] },}
 
 module.exports = nextConfig
